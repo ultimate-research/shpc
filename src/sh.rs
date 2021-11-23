cf1be84cd633749aee9398609ede896e0ddc7a1c
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-11-23 14:38:43 -0600

    add tests for coefficient compression

--- src/sh.rs
+++ src/sh.rs
@@ -3,9 +3,10 @@ use glam::Vec4;
 // Constants were determined experimentally from the uniform buffer cbuf11 in Yuzu emulator.
 // Values at index 19, 20, and 21 contain the red, green, and blue coefficients.
 // The vertex shader uses these vectors to calculate RGB ambient diffuse lighting.
+// TODO: It should be possible for decompress -> compress -> decompress to be 1:1 given the low precision (8-bit).
 const SH_MIN: [f32; 4] = [0.1481, -0.2962, -0.08551, 0.35544];
 const SH_MIN_SCALE: [f32; 4] = [0.32573, 0.32573, 0.32573, 0.28209];
-const SH_MAX_SCALE: f32 = 71.93413;
+const SH_MAX_SCALE: [f32; 4] = [83.062235, 83.062225, 83.062225, 71.93401];
 
 pub fn decompress_coefficients(unk5: f32, unk6: f32, compressed_coefficients: [u8; 4]) -> [f32; 4] {
     // Reverse the coefficients to match how they appear in the uniform buffer.
@@ -18,18 +19,26 @@ pub fn decompress_coefficients(unk5: f32, unk6: f32, compressed_coefficients: [u
     );
 
     let min_value = Vec4::from(SH_MIN) + Vec4::from(SH_MIN_SCALE) * unk5;
-    let scale = SH_MAX_SCALE * unk6;
-    (min_value + t * scale).to_array()
+    let scale = Vec4::from(SH_MAX_SCALE) * unk6;
+    (t * scale + min_value).to_array()
 }
 
-// TODO: Define the inverse operation?
-fn compress_coefficients(unk5: f32, unk6: f32, coefficients: [f32; 4]) -> [u8; 4] {
-    [0u8; 4]
+pub fn compress_coefficients(unk5: f32, unk6: f32, coefficients: [f32; 4]) -> [u8; 4] {
+    let t = Vec4::from(coefficients);
+
+    let min_value = Vec4::from(SH_MIN) + Vec4::from(SH_MIN_SCALE) * unk5;
+    let scale = Vec4::from(SH_MAX_SCALE) * unk6;
+    let buffer = (t - min_value) / scale * 255.0;
+
+    // Reverse the coefficients to match how they appear in the shpcanim file.
+    // TODO: Skip the reversing?
+    let [b3, b2, b1, b0] = buffer.to_array();
+    [b0 as u8, b1 as u8, b2 as u8, b3 as u8]
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::decompress_coefficients;
+    use super::*;
     use approx::relative_eq;
 
     macro_rules! assert_almost_eq {
@@ -66,6 +75,10 @@ mod tests {
             decompress_coefficients(0.0, 2.0, [255, 0, 0, 0])
         );
         assert_almost_eq!(
+            [83.53618, 83.09192, 83.30261, 72.57165],
+            decompress_coefficients(1.0, 1.0, [255, 255, 255, 255])
+        );
+        assert_almost_eq!(
             [-0.17762, -0.62195, -0.41125, 0.07337],
             decompress_coefficients(-1.0, 1.0, [0, 0, 0, 0])
         );
@@ -88,6 +101,14 @@ mod tests {
         assert_almost_eq!(
             [82.88475, 82.44044, 82.65115, 0.07334],
             decompress_coefficients(-1.0, 1.0, [0, 255, 255, 255])
+        );
+        assert_almost_eq!(
+            [0.1481, -0.2962, -0.08551, 0.35544],
+            decompress_coefficients(0.0, 0.0, [255, 255, 255, 255])
+        );
+        assert_almost_eq!(
+            [166.27257, 165.82825, 166.03894, 144.22346],
+            decompress_coefficients(0.0, 2.0, [255, 255, 255, 255])
         );
         assert_almost_eq!(
             [-0.17764, -0.62193, -0.41124, 72.00748],
@@ -124,6 +145,102 @@ mod tests {
         assert_almost_eq!(
             [-0.17763, -0.62194, -0.41124, 36.18145],
             decompress_coefficients(-1.0, 1.0, [128, 0, 0, 0])
+        );
+    }
+
+    #[test]
+    fn compress_sh_coefficients() {
+        assert_eq!(
+            [128, 0, 0, 0],
+            compress_coefficients(1.0, 1.0, [-0.17764, -0.62193, -0.41124, 36.18145])
+        );
+        assert_eq!(
+            [0, 128, 128, 128],
+            compress_coefficients(-1.0, 1.0, [41.51643, 41.07212, 41.28282, 0.07334])
+        );
+        assert_eq!(
+            [255, 0, 0, 0],
+            compress_coefficients(0.0, 2.0, [0.1481, -0.2962, -0.08551, 144.22372])
+        );
+        assert_eq!(
+            [128, 0, 0, 0],
+            compress_coefficients(0.0, 2.0, [0.1481, -0.2962, -0.08551, 72.57165])
+        );
+        assert_eq!(
+            [255, 0, 0, 0],
+            compress_coefficients(1.0, 1.0, [0.47383, 0.02954, 0.24023, 72.57165])
+        );
+        assert_eq!(
+            [0, 255, 255, 255],
+            compress_coefficients(-1.0, 1.0, [82.88475, 82.44044, 82.65115, 0.07334])
+        );
+        assert_eq!(
+            [255, 0, 0, 0],
+            compress_coefficients(-1.0, 1.0, [-0.17763, -0.62194, -0.41124, 72.00747])
+        );
+        assert_eq!(
+            [128, 0, 0, 0],
+            compress_coefficients(1.0, 1.0, [0.47383, 0.02954, 0.24023, 36.74563])
+        );
+        assert_eq!(
+            [0, 0, 0, 0],
+            compress_coefficients(0.0, 2.0, [0.1481, -0.2962, -0.08551, 0.35544])
+        );
+        assert_eq!(
+            [0, 0, 0, 0],
+            compress_coefficients(-1.0, 1.0, [-0.17764, -0.62193, -0.41124, 0.07334])
+        );
+        assert_eq!(
+            [0, 0, 0, 0],
+            compress_coefficients(-1.0, 1.0, [-0.17762, -0.62195, -0.41125, 0.07337])
+        );
+        assert_eq!(
+            [255, 0, 0, 0],
+            compress_coefficients(1.0, 1.0, [-0.17764, -0.62193, -0.41124, 72.00748])
+        );
+        assert_eq!(
+            [255, 255, 255, 255],
+            compress_coefficients(0.0, 0.0, [0.1481, -0.2962, -0.08551, 0.35544])
+        );
+        assert_eq!(
+            [128, 0, 0, 0],
+            compress_coefficients(-1.0, 1.0, [-0.17763, -0.62194, -0.41124, 36.18145])
+        );
+        assert_eq!(
+            [255, 0, 0, 0],
+            compress_coefficients(0.0, 1.0, [0.1481, -0.2962, -0.08551, 72.28955])
+        );
+        assert_eq!(
+            [255, 255, 255, 255],
+            compress_coefficients(1.0, 1.0, [83.53618, 83.09192, 83.30261, 72.57165])
+        );
+        assert_eq!(
+            [128, 0, 0, 0],
+            compress_coefficients(0.0, 1.0, [0.1481, -0.2962, -0.08551, 36.46354])
+        );
+        assert_eq!(
+            [0, 0, 0, 0],
+            compress_coefficients(-1.0, 1.0, [-0.17763, -0.62194, -0.41124, 0.07336])
+        );
+        assert_eq!(
+            [255, 255, 255, 255],
+            compress_coefficients(0.0, 2.0, [166.27257, 165.82825, 166.03894, 144.22346])
+        );
+        assert_eq!(
+            [0, 0, 0, 0],
+            compress_coefficients(0.0, 1.0, [0.1481, -0.2962, -0.08551, 0.35544])
+        );
+        assert_eq!(
+            [0, 0, 0, 0],
+            compress_coefficients(1.0, 1.0, [0.47383, 0.02954, 0.24023, 0.63753])
+        );
+        assert_eq!(
+            [0, 0, 0, 0],
+            compress_coefficients(-1.0, 1.0, [-0.17763, -0.62194, -0.41124, 0.07335])
+        );
+        assert_eq!(
+            [255, 255, 255, 255],
+            compress_coefficients(-1.0, 1.0, [82.88475, 82.44044, 82.65115, 72.00748])
         );
     }
 }
